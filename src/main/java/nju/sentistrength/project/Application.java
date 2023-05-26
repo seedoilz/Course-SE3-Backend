fc63878a78690003074e7e74466644fa4a1a01d5
yuhaofeng <2087953253@qq.com>
2023-05-26 18:54:01 +0800

    调整代码

--- src/main/java/nju/sentistrength/project/Application.java
+++ src/main/java/nju/sentistrength/project/Application.java
@@ -1,10 +1,28 @@
 package nju.sentistrength.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
 
 @SpringBootApplication()
 public class Application {
+
+    public static void disableWarning() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe u = (Unsafe) theUnsafe.get(null);
+
+            Class cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field logger = cls.getDeclaredField("logger");
+            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
     public static void main(String[] args) {
+        disableWarning();
         SpringApplication.run(Application.class, args);
     }
 }
